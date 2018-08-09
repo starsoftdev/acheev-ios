68ee5f6bd3b46be2129ab481ed116edf082942d1
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-08-08 20:55:58 -0700

    Rearranged files.

---
+++ Acheev/Acheev/API/APIService.swift
@@ -0,0 +1,18 @@
+//
+//  APIService.swift
+//  Acheev
+//
+//  Created by Roman Salazar Lopez on 8/8/18.
+//  Copyright © 2018 Roman Salazar Lopez. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    
+    static let shared = APIService()
+    
+    let baseURL = ""
+    
+}
